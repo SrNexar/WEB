c37014db68384dd222f6b44b5165371ff7dec403
SrNexar <nfsalguero@uce.edu.ec>
2024-12-02 10:16:47 -0500

    Comprobacion

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Mi primera página web en Python"
+    return "Mi primera página web desplegada en AWS :)"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
